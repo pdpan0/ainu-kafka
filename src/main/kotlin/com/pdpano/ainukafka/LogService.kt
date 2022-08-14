96f78cf0b05c01db1be734f3303abc5ca53c0fed
pdpan0 <lm.m4rtins@gmail.com>
2022-08-14 16:52:42 -0300

    FEAT: Aplicando uma configuração adicional para LogService, devido o mesmo não ter uma classe especifica para as mensagens.

--- src/main/kotlin/com/pdpano/ainukafka/LogService.kt
+++ src/main/kotlin/com/pdpano/ainukafka/LogService.kt
@@ -1,13 +1,18 @@
 package com.pdpano.ainukafka
 
 import com.pdpano.ainukafka.config.KafkaConfiguration
+import org.apache.kafka.clients.consumer.ConsumerConfig
+import org.apache.kafka.common.serialization.StringDeserializer
 import java.time.Duration
 import java.util.regex.Pattern
 
 class LogService { }
 
 fun main() {
-    val consumer = KafkaConfiguration.initConsumer<String, String>(LogService::class.simpleName!!)
+    val consumer = KafkaConfiguration.initConsumer<String, String>(
+        LogService::class.simpleName!!,
+        mapOf(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG to StringDeserializer::class.java.name)
+    )
     consumer.subscribe(Pattern.compile("AINU.*"))
 
     while (true) {
@@ -15,7 +20,7 @@ fun main() {
 
         if (!records.isEmpty) {
             records.forEach {
-                println("${it.topic()} :: ${it.key()}")
+                println("${it.topic()} :: ${it.key()} :: ${it.value()}")
             }
         }
     }
