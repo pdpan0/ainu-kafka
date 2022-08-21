24cb14c805c857078bb466d7f7e6c17e4bd49304
pdpan0 <lm.m4rtins@gmail.com>
2022-08-21 20:27:38 -0300

    REFACT: Alterando toda a estrutura do projeto, para adicionar módulos para cada tipo de serviço específico.

--- common-kafka/src/main/kotlin/com/pdpano/ainukafka/config/GsonDeserializer.kt
+++ common-kafka/src/main/kotlin/com/pdpano/ainukafka/config/GsonDeserializer.kt
@@ -1,11 +1,11 @@
 package com.pdpano.ainukafka.config
 
-import com.google.gson.GsonBuilder
 import org.apache.kafka.common.serialization.Deserializer
 import java.lang.RuntimeException
+
 class GsonDeserializer<T>: Deserializer<T> {
     private var type: Class<T>? = null
-    private val gson = GsonBuilder().create()
+    private val gson = com.google.gson.GsonBuilder().create()
 
     companion object {
         const val TYPE_CONFIG: String = "com.pdpano.ainukafka.type_config"
@@ -25,4 +25,3 @@ class GsonDeserializer<T>: Deserializer<T> {
     }
 
 }
-
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,3 +1,7 @@
-
 rootProject.name = "ainu-kafka"
 
+include("service-email")
+include("service-log")
+include("service-fraud-detector")
+include("service-send-order")
+include("common-kafka")
